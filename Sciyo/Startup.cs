08cd0dbd216a1d762dcf7e7414c3eab2dc11fa70
goh-chunlin <chunlin@live.com.sg>
2017-09-05 23:02:39 +0800

    Added DbContext.

--- Sciyo/Startup.cs
+++ Sciyo/Startup.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sciyo.Data;
+using Sciyo.Data.VideoEntity;
 
 namespace Sciyo
 {
@@ -21,7 +24,13 @@ namespace Sciyo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options => options.
+                UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IVideoRepository, VideoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
