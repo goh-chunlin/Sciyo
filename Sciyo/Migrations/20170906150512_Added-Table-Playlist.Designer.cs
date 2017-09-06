022de5508be87882c5dd880dd2631a1a7df790af
goh-chunlin <chunlin@live.com.sg>
2017-09-06 23:07:42 +0800

    Added migration script for adding table Playlist.

--- Sciyo/Migrations/20170906150512_Added-Table-Playlist.Designer.cs
+++ Sciyo/Migrations/20170906150512_Added-Table-Playlist.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Sciyo.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20170906150512_Added-Table-Playlist")]
+    partial class AddedTablePlaylist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Sciyo/Migrations/20170906150512_Added-Table-Playlist.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Sciyo.Migrations
+{
+    public partial class AddedTablePlaylist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PlaylistId",
+                table: "Video",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Playlist",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlist", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Video_PlaylistId",
+                table: "Video",
+                column: "PlaylistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Video_Playlist_PlaylistId",
+                table: "Video",
+                column: "PlaylistId",
+                principalTable: "Playlist",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Video_Playlist_PlaylistId",
+                table: "Video");
+
+            migrationBuilder.DropTable(
+                name: "Playlist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Video_PlaylistId",
+                table: "Video");
+
+            migrationBuilder.DropColumn(
+                name: "PlaylistId",
+                table: "Video");
+        }
+    }
+}
